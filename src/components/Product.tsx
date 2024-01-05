bb013c1835905280298ff592d1911aa53f37e46c
Cuong <cuong@foxit.co.jp>
2024-01-05 16:07:40 +0700

    init

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -3,10 +3,9 @@ import React, { useState } from "react";
 import Link from "next/link";
 import { FormatUtils } from "@/utils/format";
 import { shopping } from "@/assets";
-import { showQuickView,} from "@/store/slices/quickViewProduct";
+import { showQuickView } from "@/store/slices/quickViewProduct";
 import { useDispatch } from "react-redux";
-
-
+import testProduct from "@/assets/test_product.jpg";
 interface propsInterface {
   link: string;
   image: string;
@@ -22,7 +21,7 @@ const Product = (props: propsInterface) => {
 
   const handleQuickView = () => {
     console.log("QUICK VIEW CALL");
-    dispatch(showQuickView())
+    dispatch(showQuickView());
 
     setQuickView(!quickView);
   };
@@ -32,16 +31,16 @@ const Product = (props: propsInterface) => {
 
   return (
     <>
-      <div className="relative group/item hover:bg-slate-100 ">
-        <Link href={link} className=" h-full flex flex-col  mb-2 cursor-pointer hover:text-red text-white">
-          <img src={image} alt={title} className="h-full bg-gray object-contain hover:-translate-y-1 hover:scale-105 duration-300 rounded"></img>
-          <p className="mt-2 px-2 text-black font-semibold text-lg"> {title}</p>
-          <p className="mt-2 px-2 text-red font-bold">
+      <div className="border border-[#EDEDED] relative group/item p-4 rounded-lg ">
+        <Link href={link} className=" h-full flex flex-col  mb-2 cursor-pointer hover:text-red text-white gap-4">
+          <img src={testProduct.src} alt={title} className="h-full bg-gray object-contain hover:-translate-y-1 hover:scale-105 duration-300 "></img>
+          <p className="text-black font-semibold text-lg"> {title}</p>
+          <p className="text-red font-bold">
             {FormatUtils.formatPriceVND(Number(price))} <del className="text-gray"> {FormatUtils.formatPriceVND(Number(beforeDiscount))}</del>
           </p>
         </Link>
         <button
-          className="transition-all ease-in-out duration-300  group-hover/item:top-[38%]  invisible group-hover/item:visible  absolute text-red top-[-100%] left-[10%] translate-x-[-50%] translate-y-[-180%] p-2 bg-red text-white rounded"
+          className="transition-all ease-in-out duration-300  group-hover/item:top-[34%]  invisible group-hover/item:visible  absolute text-red top-[-100%] left-[10%] translate-x-[-50%] translate-y-[-180%] p-2 bg-red text-white rounded"
           onClick={handleAddToCart}
         >
           <img src={shopping.src} className="max-w-[20px] "></img>
