fc3175eddcb0d4145fb79ef62b4b790af2721e7a
Cuong <cuong@foxit.co.jp>
2024-01-23 17:05:38 +0700

    edit script

--- backend/src/script/createDB.js
+++ backend/src/script/createDB.js
@@ -4,7 +4,7 @@
 
 
 var conn = new Mongo();
-var db = connect("localhost:27017/fishingDB");
+var db = connect("localhost:27017/viahe");
 // db.categories.insertMany([
 //    {
 //       title:"Cần câu cá",
@@ -36,45 +36,45 @@ db.users.insertMany([
 ])
 
 db.products.insertMany( [
-    {
-        name: 'Tơ nhện mini',
-        description:"HEHE",
-        price:100000,
-        quatity:100,
-        images:[],
-        creatorPerson:"",
-        dateCreated:Date(),
-        dateModify:Date(),
-        category:"Tơ nhện",
-        sizes:[]
+//     {
+//         name: 'Tơ nhện mini',
+//         description:"HEHE",
+//         price:100000,
+//         quatity:100,
+//         images:[],
+//         creatorPerson:"",
+//         dateCreated:Date(),
+//         dateModify:Date(),
+//         category:"Tơ nhện",
+//         sizes:[]
 
-    },
-    {
-      name: 'Cần câu test',
-      description:"HEHE",
-      price:null,
-      quatity:null,
-      images:[],
-      comments:[],
-      creatorPerson:"",
-      dateCreated:Date(),
-      dateModify:Date(),
-      category:"Cần câu tay",
-      sizes:[
-         {
-            size:"4h",
-            quatity:100,
-            price:100000
-         },
-         {
-            size:"8h",
-            quatity:90,
-            price:100000
-         },
+//     },
+//     {
+//       name: 'Cần câu test',
+//       description:"HEHE",
+//       price:null,
+//       quatity:null,
+//       images:[],
+//       comments:[],
+//       creatorPerson:"",
+//       dateCreated:Date(),
+//       dateModify:Date(),
+//       category:"Cần câu tay",
+//       sizes:[
+//          {
+//             size:"4h",
+//             quatity:100,
+//             price:100000
+//          },
+//          {
+//             size:"8h",
+//             quatity:90,
+//             price:100000
+//          },
 
-      ]
+//       ]
 
-  },
+//   },
    
  ] )
 
